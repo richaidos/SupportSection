5a28d0c3c5070db0ddd69aa1800a15f995e4d229
richaidos <aidossuper@gmail.com>
2022-02-28 01:07:58 +0600

    Добавил пакет контроллеров

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/richaidos/SupportSection/controller"
 	d "github.com/richaidos/SupportSection/database"
 	g "github.com/richaidos/SupportSection/globals"
 )
@@ -20,6 +21,11 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	v1 := r.Group("/api")
+	{
+		v1.GET("search", controller.SearchByQuery)
+	}
 
 	r.Run(g.ISettings.Config.Port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
